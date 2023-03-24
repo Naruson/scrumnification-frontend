e79e4439f8732bbaed7d231c1cdc876443767054
thanapatch <63160240@go.buu.ac.th>
2023-03-24 10:58:11 +0700

    หน้าสมาชิกมกุล

--- src/components/cluster-task.vue
+++ src/components/cluster-task.vue
@@ -16,53 +16,192 @@ console.log(clusterId);
 
 const cluster = useCluster
 onMounted(() => {
-    role = localStorage.getItem('role');
-    console.log('role: '+ role);
-    cluster.dispatch("getClusterTasks", {clusterId: clusterId});
+    cluster.dispatch("getClusterTasks", { clusterId: clusterId });
 });
 </script>   
 <template>
-        <div class="box center">
-            <font-awesome-icon icon="fa-solid fa-list-check" />
-            <router-link v-bind:to="'/cluster/'+clusterId+'/check-task'" v-if="role == 'coach'">
-                <button class="btn color-white"> 
-                  <div class="btn-center">
-                    <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-list-check" viewBox="0 0 16 16">
-                    <path fill-rule="evenodd" d="M5 11.5a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h9a.5.5 0 0 1 0 1h-9a.5.5 0 0 1-.5-.5zM3.854 2.146a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 1 1 .708-.708L2 3.293l1.146-1.147a.5.5 0 0 1 .708 0zm0 4a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 1 1 .708-.708L2 7.293l1.146-1.147a.5.5 0 0 1 .708 0zm0 4a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 0 1 .708-.708l.146.147 1.146-1.147a.5.5 0 0 1 .708 0z"/>
-                  </svg>
-                  check task
-                  </div>
-                </button>
-            </router-link>
-            <div class="head1">Task</div>
-                <div class="d-flex justify-content-end se">
-                    <label for="input SE-Dollar" class="col-sm-2 col-form-label pl">SE Dollar:</label>
-                    <div class="se-box">
-                      {{ cluster.state.point }}
+    <div>
+      <div class="row">
+        <div class="card col-6" style="">
+            <div class="row g-0">
+                <div class="col-sm-5">
+                    <img src="https://humancatsworld.files.wordpress.com/2019/03/cats.jpg?w=825" class="card-img-top h-75 w-100 card-image" alt="...">
+                </div>
+                <div class="col-sm-7">
+                    <div class="card-body">
+                        <p>ชื่อ-สกุล :</p> 
+                        <p>รหัสนิสิต :</p>
+                        <p class="position">ตำแหน่ง :</p>
+                        
                     </div>
                 </div>
-            <table class="table table-striped">
-                <thead>
-                    <tr style="background-color: #0052D4; color: rgb(255, 255, 255);">
-                    <th scope="col">No.</th>
-                    <th scope="col">Name</th>
-                    <th scope="col">Date</th>
-                    <th scope="col">SE Dollar</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr v-for="(item, index) in cluster.state.tasksTransaction" :key="item._id">
-                    <td scope="row">{{ index + 1 }}</td>
-                    <td>{{ item.name }}</td>
-                    <td>{{ item.created_at }}</td>
-                    <td>{{ item.point }}</td>
-                    </tr>
-
-                </tbody>
-            </table>
+            </div>
         </div>
-</template>
+        <div class="card col-6" style="">
+          <div class="row g-0">
+                <div class="col-sm-5">
+                    <img src="https://humancatsworld.files.wordpress.com/2019/03/cats.jpg?w=825" class="card-img-top h-75 w-100 card-image" alt="...">
+                </div>
+                <div class="col-sm-7">
+                    <div class="card-body">
+                        <p>ชื่อ-สกุล</p> 
+                        <p>รหัสนิสิต</p>
+                        <p class="position">ตำแหน่ง</p>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="card col-6" style="">
+            <div class="row g-0">
+                <div class="col-sm-5">
+                    <img src="https://humancatsworld.files.wordpress.com/2019/03/cats.jpg?w=825" class="card-img-top h-75 w-100 card-image" alt="...">
+                </div>
+                <div class="col-sm-7">
+                    <div class="card-body">
+                        <p>ชื่อ-สกุล</p> 
+                        <p>รหัสนิสิต</p>
+                        <p class="position">ตำแหน่ง</p>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="card col-6" style="">
+            <div class="row g-0">
+                <div class="col-sm-5">
+                    <img src="https://humancatsworld.files.wordpress.com/2019/03/cats.jpg?w=825" class="card-img-top h-75 w-100 card-image" alt="...">
+                </div>
+                <div class="col-sm-7">
+                    <div class="card-body">
+                        <p>ชื่อ-สกุล</p> 
+                        <p>รหัสนิสิต</p>
+                        <p class="position">ตำแหน่ง</p>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+      </div>
+      <div class="row">
+        <div class="card col-6" style="">
+            <div class="row g-0">
+                <div class="col-sm-5">
+                    <img src="https://humancatsworld.files.wordpress.com/2019/03/cats.jpg?w=825" class="card-img-top h-75 w-100 card-image" alt="...">
+                </div>
+                <div class="col-sm-7">
+                    <div class="card-body">
+                        <p>ชื่อ-สกุล</p> 
+                        <p>รหัสนิสิต</p>
+                        <p class="position">ตำแหน่ง</p>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="card col-6" style="">
+            <div class="row g-0">
+                <div class="col-sm-5">
+                    <img src="https://humancatsworld.files.wordpress.com/2019/03/cats.jpg?w=825" class="card-img-top h-75 w-100 card-image" alt="...">
+                </div>
+                <div class="col-sm-7">
+                    <div class="card-body">
+                        <p>ชื่อ-สกุล</p> 
+                        <p>รหัสนิสิต</p>
+                        <p class="position">ตำแหน่ง</p>
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+      </div>
+    </div>
+  </template>
 
 <style scoped>
 @import '@/assets/styles/cluster-task.css';
+
+.invi-box {
+    height: 60px;
+    color: aqua;
+}
+
+/* .container{
+    background-color: brown;
+    z-index: 1000;
+} */
+
+/* Option 2: Import via CSS */
+@import url("https://cdn.jsdelivr.net/npm/bootstrap-icons@1.3.0/font/bootstrap-icons.css");
+
+.container {
+    height: auto;
+    padding-top: 30px;
+    padding-left: 170px;
+    margin: 20px;
+    min-height: 100px;
+    max-height: 1000px;
+    overflow: auto;
+    border-style: solid;
+    border-width: 2px;
+    background-clip: padding-box;
+    border-color: #0052D4;
+    border-radius: 15px;
+    flex-direction: column-reverse;
+    /* display: flex;*/
+    justify-content: center;
+}
+
+.card {
+    width: 530px;
+    height: 200px;
+    /* padding: auto; */
+    margin: 15px;
+    min-height: 150px;
+    max-height: 500px;
+    max-width: 530;
+    border-color: #0052D4;
+    /* align-items: center; */
+    border-radius: 10px;
+    box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+    padding: 20px 50px;
+    display: flex;
+    justify-content: space-around;
+}
+
+.card-image {
+    flex: 0 0 auto;
+    width: 150px;
+    height: 100% !important;
+    margin-right: 20px;
+    border-radius: 50%;
+    /* overflow: hidden; */
+}
+
+/* .card-image img {
+    display: block;
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
+} */
+
+/* .card-details {
+    flex: 1 1 auto;
+    align-items: flex-end;
+} */
+
+/* .card-details h5 {
+    margin-top: 10px;
+    margin-bottom: 10px;
+} */
+
+/* .card-details p {
+    margin: 30;
+} */
+
+.position {
+    color: #0052D4;
+}
 </style>
